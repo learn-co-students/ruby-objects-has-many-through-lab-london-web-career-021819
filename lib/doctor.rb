9ad9ff33a687587453beee6aa1cb0397a6895213
CalieR <calielrushton@gmail.com>
2019-02-22 19:11:01 +0000

    Done.

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -13,5 +13,12 @@ class Doctor
     @@all
   end
 
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
+
+  def appointments
+    Appointment.all.select { |appt| appt.doctor == self }
+  end
 
 end
